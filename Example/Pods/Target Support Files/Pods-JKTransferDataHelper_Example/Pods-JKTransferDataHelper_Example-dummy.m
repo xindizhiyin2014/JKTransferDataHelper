ca0c49e7a63d3245d71bc93dcc9ed57babea18a3
HHL110120 <929097264@qq.com>
2018-12-17 19:13:10 +0800

    init

---
+++ Example/Pods/Target Support Files/Pods-JKTransferDataHelper_Example/Pods-JKTransferDataHelper_Example-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_JKTransferDataHelper_Example : NSObject
+@end
+@implementation PodsDummy_Pods_JKTransferDataHelper_Example
+@end
---
+++ Example/Pods/Target Support Files/Pods-JKTransferDataHelper_Tests/Pods-JKTransferDataHelper_Tests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_JKTransferDataHelper_Tests : NSObject
+@end
+@implementation PodsDummy_Pods_JKTransferDataHelper_Tests
+@end
